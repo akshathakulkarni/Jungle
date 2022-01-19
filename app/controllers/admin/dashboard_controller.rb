ae0bbdbdc697ea5543afb418e60b96962217379f
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-01-18 19:07:36 -0500

    Store the name and password in .env file and use EVN to refer them for login credentials

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
   end
