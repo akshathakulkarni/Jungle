6685b5f5ef0deed8aa9db7296b9efe3791d3d213
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-01-28 00:52:21 -0500

    Add validations for name, email and password for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true, length: { minimum: 3 }
+  
+  def authenticate_with_credentials(email, password)
+    if user.authenticate(email, password)
+      @user
+    else
+      nil
+    end
+  end
+
+  
 end
