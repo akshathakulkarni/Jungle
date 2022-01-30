8ff25fcaa0ba878c9a93ba5eeb9376d7f133d59b
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-01-29 23:41:54 -0500

    Redefine the authenticate_with_credentials class method to check for email's lettercase and white spaces before authenticating

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,28 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, confirmation: true, length: { minimum: 3 }
+  # before_save { self.email.downcase! }
+
+   def downcase_fields
+      self.email.downcase!
+   end
+
+   def remove_space
+      self.email.squish
+   end
   
-  def authenticate_with_credentials(email, password)
-    if user.authenticate(email, password)
+  def self.authenticate_with_credentials(email, password)
+    if email == email.upcase! 
+      correct_email = email.downcase!
+    else
+      correct_email = email
+    end
+    # Use squish to remove white spaces if any
+    @user = User.find_by email:correct_email.squish 
+    if @user.authenticate(password)
       @user
     else
       nil
     end
   end
-
-  
 end
